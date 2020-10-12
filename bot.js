3c9933ed14195432a870d96ba9a76b291823ddb6
DolgushinG <40915075+DolgushinG@users.noreply.github.com>
2020-10-12 17:52:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 
 function doPost(e){
-  Logger = BetterLog.useSpreadsheet('');
+  Logger = BetterLog.useSpreadsheet(ssId());
   let update = JSON.parse(e.postData.contents);
   checkRole(update);
   }
